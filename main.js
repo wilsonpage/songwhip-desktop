7968d12e4d5b6665a28876ff72e1bc76eb751fe0
Wilson Page <wilsonpage@me.com>
2018-07-19 08:08:59 +0100

    First commit

--- main.js
+++ main.js
@@ -2,8 +2,19 @@
 
 var path = require('path')
 var electron = require('electron');
+const debug = require('debug')('songwhip:main');
 var BrowserWindow = electron.BrowserWindow;
-const {app, Menu, Tray} = require('electron');
+const {app, Menu, MenuItem, Tray, clipboard, shell } = require('electron');
+
+const clipboardListener = require('electron-clipboard-extended')
+const { NotificationCenter } = require('node-notifier');
+
+
+const createMessage = require('./lib/create-message');
+const songwhip = require('./lib/songwhipify');
+const getLink = require('./lib/get-link');
+
+var notifier = new NotificationCenter();
 
 var trayIcon = null;
 var window = null;
@@ -15,27 +26,26 @@ const HORIZ_PADDING = 15;
 const VERT_PADDING = 15;
 
 app.on('ready', function() {
-  
-  if(process.platform === 'darwin') app.dock.hide();
+  if (process.platform === 'darwin') app.dock.hide();
 
-  window = new BrowserWindow({
-    width: WINDOW_WIDTH,
-    height: WINDOW_HEIGHT,
-    resizable: false,
-    frame: false,
-    transparent: true,
-    show: false
-  });
+  // window = new BrowserWindow({
+  //   width: WINDOW_WIDTH,
+  //   height: WINDOW_HEIGHT,
+  //   resizable: false,
+  //   frame: false,
+  //   transparent: true,
+  //   show: false
+  // });
 
-  window.loadURL('file://' + __dirname + '/index.html');
+  // window.loadURL('file://' + __dirname + '/index.html');
 
-  window.on('close', function () {
-    window = null;
-  });
+  // window.on('close', function () {
+  //   window = null;
+  // });
 
-  window.on('blur', function(){
-    window.hide();
-  });
+  // window.on('blur', function(){
+  //   window.hide();
+  // });
 
   const iconName = 'images/icon.png';
   const iconPath = path.join(__dirname, iconName);
@@ -43,40 +53,61 @@ app.on('ready', function() {
   trayIcon = new Tray(iconPath);
   trayIcon.setToolTip('Hello World');
 
-  trayIcon.on('click', (event) => {
-    var screen = electron.screen;
-    const cursorPosition = screen.getCursorScreenPoint();
-    const primarySize = screen.getPrimaryDisplay().workAreaSize;
-    const trayPositionVert = cursorPosition.y >= primarySize.height/2 ? 'bottom' : 'top';  
-    const trayPositionHoriz = cursorPosition.x >= primarySize.width/2 ? 'right' : 'left';  
-    window.setPosition(getTrayPosX(),  getTrayPosY());
-    window.isVisible() ? window.hide() : window.show();
+  clipboardListener
+    .on('text-changed', async () => {
+      debug('clipboard change')
+        const link = getLink(clipboard.readText());
+        if (!link) {
+          debug('no link in clipboard text');
+          return;
+        }
 
-    function getTrayPosX() {
-      const horizBounds = {
-        left:   cursorPosition.x - WINDOW_WIDTH/2,
-        right:  cursorPosition.x + WINDOW_WIDTH/2
-      }
-      if (trayPositionHoriz == 'left') {
-        return horizBounds.left <= HORIZ_PADDING ? HORIZ_PADDING : horizBounds.left;
-      }
-      else {
-        return horizBounds.right >= primarySize.width ? primarySize.width - HORIZ_PADDING - WINDOW_WIDTH: horizBounds.right - WINDOW_WIDTH;
-      }
-    }    
-    function getTrayPosY() {
-      return trayPositionVert == 'bottom' ? cursorPosition.y - WINDOW_HEIGHT - VERT_PADDING : cursorPosition.y + VERT_PADDING;
-    }
-  });
+        const json = await songwhip(link);
 
-  const {Menu, MenuItem} = require('electron');
+        if (!json) {
+          debug('no songwhip data');
+          return;
+        }
+
+        const title = createMessage(json);
+        const body = 'Click to copy';
+
+        notifier.notify({
+          title,
+          message: body,
+          icon: undefined,
+          sound: false, // Only Notification Center or Windows Toasters
+          timeout: 20,
+          // open: json.url,
+          closeLabel: 'Close',
+          actions: 'Show',
+        },
+        function(err, response, { activationValue }) {
+          debug('notification callback', err, response, activationValue);
+          if (activationValue === 'Show') shell.openExternal(json.url);
+        });
+
+        notifier.on('click', (notifierObject, options) => {
+          debug('notification click', options);
+          clipboard.writeText(json.url);
+        });
+    })
+    .startWatching();
+
+  // trayIcon.on('click', (event) => {
+  //   menu.popup(window);
+  // });
+
   var menu = new Menu();
-
+  menu.append(new MenuItem({ label: 'A', click: () => app.quit() }));
+  menu.append(new MenuItem({ label: 'B', click: () => app.quit() }));
   menu.append(new MenuItem({ label: 'Quit', click: () => app.quit() }));
 
-  var ipcMain = require('electron').ipcMain;
-  ipcMain.on('show-config-menu', (event) => {
-      menu.popup(window);
-  });
+  trayIcon.setContextMenu(menu);
+
+  // var ipcMain = require('electron').ipcMain;
+  // ipcMain.on('show-config-menu', (event) => {
+  //     menu.popup(window);
+  // });
 
 });
